f9b67f52e6a75b6a2fc0960f355bc73474eb7d29
Élon Castro <elon@ecgroup.com.br>
2021-12-13 12:22:18 -0300

    Implementação básica dos atributos e métodos da classe CarrinhoCompra para aplicação dos princípios SOLID.

--- app_carrinho_compras/index.php
+++ app_carrinho_compras/index.php
@@ -4,4 +4,27 @@ require __DIR__ . "/vendor/autoload.php";
 
 use src\CarrinhoCompra;
 
-echo CarrinhoCompra::teste();
+$carrinho1 = new CarrinhoCompra();
+print '<pre>'; print_r($carrinho1->exibirItens()); print '</pre><br>';
+print 'Valor total: ' . $carrinho1->exibirValorTotal() . '<br>';
+print 'Status do pedido: ' . $carrinho1->exibirStatus() . '<br>';
+
+/*
+$carrinho1->adicionarItem('Bicicleta', 750.50);
+$carrinho1->adicionarItem('Geladeira', 1250.73);
+$carrinho1->adicionarItem('Tapete', 350.25);
+*/
+
+print '<pre>'; print_r($carrinho1->exibirItens()); print '</pre><br>';
+print 'Valor total recalculado: ' . $carrinho1->exibirValorTotal() . '<br>';
+print 'Status do pedido: ' . $carrinho1->exibirStatus() . '<br>';
+
+$carrinho1->adicionarItem('Televisão "', 3725.18);
+
+if($carrinho1->confirmarPedido()){
+    print 'Pedido realizado com sucesso<br>';
+}else{
+    print 'Erro na confirmação do pedido<br>';
+}
+
+print 'Status do pedido: ' . $carrinho1->exibirStatus() . '<br>';
--- app_carrinho_compras/src/CarrinhoCompra.php
+++ app_carrinho_compras/src/CarrinhoCompra.php
@@ -3,7 +3,58 @@
 namespace src;
 
 class CarrinhoCompra {
-    public static function teste() {
-        return 'ok';
+    
+    // Atributos
+    private $itens;
+    private $status;
+    private $valorTotal;
+    
+    // Método construtor
+    public function __construct(){
+        $this->itens = [];
+        $this->status = 'aberto';
+        $this->valorTotal = 0.00;
+    }
+
+    // Retorna conteúdo do atributo itens constantes no objeto.
+    public function exibirItens(){
+        return $this->itens;
+    }
+
+    // Adiciona itens no atributo, seguindo o padrão: Descrição e Valor.
+    public function adicionarItem(string $item, float $valor){
+        array_push($this->itens, ["item"=>$item, "valor"=>$valor]);
+        $this->valorTotal += $valor;
+        return true;
+    }
+
+    // Retorna valor total dos itens no carrinho
+    public function exibirValorTotal(){
+        return $this->valorTotal;
+    }
+
+    // Retorna status do carrinho
+    public function exibirStatus(){
+        return $this->status;
+    }
+
+    // Confirma pedido: alteração do status para confirmado
+    public function confirmarPedido(){
+        if($this->validarCarrinho()){
+            $this->status = 'confirmado';
+            $this->enviarEmailConfirmacao();
+            return true;
+        }
+        return false;
+    }
+
+    // Envia e-mail de confirmação do pedido
+    public function enviarEmailConfirmacao(){
+        print '<br>...E-mail de confirmação enviado...<br>';
+    }
+
+    // Verifica se carrinho está vazio
+    public function validarCarrinho(){
+        return count($this->itens) > 0;
     }
 }
