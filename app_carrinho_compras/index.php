225981b167e85fc2c1b9f274c950f27154a5d137
Élon Castro <elon@ecgroup.com.br>
2021-12-14 13:09:19 -0300

    Conectando os objetos das classes, utilizando o arquivo "index.php" como controlador, para realização de testes.

--- app_carrinho_compras/index.php
+++ app_carrinho_compras/index.php
@@ -3,28 +3,61 @@
 require __DIR__ . "/vendor/autoload.php";
 
 use src\CarrinhoCompra;
+use src\Item;
+use src\Pedido;
+use src\Email;
 
-/*
-$carrinho1 = new CarrinhoCompra();
-print '<pre>'; print_r($carrinho1->exibirItens()); print '</pre><br>';
-print 'Valor total: ' . $carrinho1->exibirValorTotal() . '<br>';
-print 'Status do pedido: ' . $carrinho1->exibirStatus() . '<br>';
+echo '<h3>Controlando projeto com SRP</h3>';
 
-$carrinho1->adicionarItem('Bicicleta', 750.50);
-$carrinho1->adicionarItem('Geladeira', 1250.73);
-$carrinho1->adicionarItem('Tapete', 350.25);
+$pedido = new Pedido();
+// ---------------------------------------------------
+$item1 = new Item();
+$item2 = new Item();
 
-print '<pre>'; print_r($carrinho1->exibirItens()); print '</pre><br>';
-print 'Valor total recalculado: ' . $carrinho1->exibirValorTotal() . '<br>';
-print 'Status do pedido: ' . $carrinho1->exibirStatus() . '<br>';
+$item1->setDescricao('Porta copo');
+$item1->setValor(4.55);
 
-$carrinho1->adicionarItem('Televisão "', 3725.18);
-
-if($carrinho1->confirmarPedido()){
-    print 'Pedido realizado com sucesso<br>';
-}else{
-    print 'Erro na confirmação do pedido<br>';
+$item2->setDescricao('Lâmpada');
+$item2->setValor(8.32);
+// ---------------------------------------------------
+echo '<h4>Pedido iniciado</h4>';
+echo '<pre>';
+print_r($pedido);
+echo '</pre>';
+// ---------------------------------------------------
+$pedido->getCarrinhoCompra()->adicionarItem($item1);
+$pedido->getCarrinhoCompra()->adicionarItem($item2);
+// ---------------------------------------------------
+echo '<h4>Pedido com itens</h4>';
+echo '<pre>';
+print_r($pedido);
+echo '</pre>';
+// ---------------------------------------------------
+echo '<h4>Itens do carrinho</h4>';
+echo '<pre>';
+print_r($pedido->getCarrinhoCompra()->getItens());
+echo '</pre>';
+// ---------------------------------------------------
+echo '<h4>Valor do pedido</h4>';
+$total = 0.00;
+foreach($pedido->getCarrinhoCompra()->getItens() as $key => $item){
+    $total += $item->getValor();
 }
-
-print 'Status do pedido: ' . $carrinho1->exibirStatus() . '<br>';
-*/
+echo $total;
+// ---------------------------------------------------
+echo '<h4>Carrinho está válido?</h4>';
+echo $pedido->getCarrinhoCompra()->validarCarrinho();
+// ---------------------------------------------------
+echo '<h4>Status do pedido</h4>';
+echo $pedido->getStatus();
+// ---------------------------------------------------
+echo '<h4>Confirmar pedido</h4>';
+echo $pedido->confirmarPedido();
+// ---------------------------------------------------
+echo '<h4>Status do pedido</h4>';
+echo $pedido->getStatus();
+// ---------------------------------------------------
+echo '<h4>E_mail de confirmação</h4>';
+if($pedido->getStatus() == 'confirmado'){
+    echo Email::enviarEmail();
+}
--- app_carrinho_compras/src/Email.php
+++ app_carrinho_compras/src/Email.php
@@ -4,4 +4,27 @@ namespace src;
 
 class Email {
 
+    // Atributos
+    private $de;
+    private $para;
+    private $assunto;
+    private $conteudo;
+
+    // Método construtor
+    public function __construct(
+        string $de = 'email@dominio.com.br',
+        string $para = '',
+        string $assunto = '',
+        string $conteudo = ''
+    ){
+        $this->de = $de;
+        $this->para = $para;
+        $this->assunto = $assunto;
+        $this->conteudo = $conteudo;
+    }
+
+    // Método estático para envio de e-mails
+    public static function enviarEmail(){
+        return '--- envia e-mail ---';
+    }
 }
