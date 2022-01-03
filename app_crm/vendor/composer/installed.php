56e0ce2e16d5a97a65a89085ca3ac67a2c16ed5b
Élon Castro <elon@ecgroup.com.br>
2022-01-02 23:59:48 -0300

    Criação da estrutura do projeto "CRM" para aplicação e prática do princípio "ISP" no acrônimo "SOLID".

---
+++ app_crm/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ec745b6cce6b963bf1f47b85a176aa9148f43db4',
+        'name' => 'elon/app_crm',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'elon/app_crm' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ec745b6cce6b963bf1f47b85a176aa9148f43db4',
+            'dev_requirement' => false,
+        ),
+    ),
+);
